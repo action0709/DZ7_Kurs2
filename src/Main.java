27c9b5a25eaef520ea90be8ceccd8166b0612f22
action0709 <action0709@mail.ru>
2023-05-23 18:11:49 +0400

    ДЗ7

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,21 @@ public class Main {
                 "one","two","three","four"));
         Set <String> numb=new HashSet<>(stringList);
         System.out.println(numb);
+        // задание 4
+        System.out.println();
+        List<String> words = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        Map<String,Integer>map=new HashMap<>();
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                Integer count = map.get(word) + 1;
+                map.put(word, count);
+            }else{
+            map.put(word,1);
 
+        }
+            
+        }
+        System.out.println(map.values());
     }
 
     }

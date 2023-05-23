99eec816dd7e16e3d4d61adcd2b3ba3db0c186cd
action0709 <action0709@mail.ru>
2023-05-23 17:55:21 +0400

    1,2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //задание 1
+      List<Integer> nums =new ArrayList<>(List.of(1, 10, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+        for (Integer num : nums) {
+            if(num % 2 ==1){
+                System.out.println(num);
+            }
+
+        }
+       // задание 2
+        System.out.println();
+        Set<Integer> set=new TreeSet<>(nums);
+        for (Integer num : set) {
+            if(num%2==0){
+                System.out.println(num);
+            }
+
+        }
+
     }
 }
